100187b64fc218e49de716c217f98a57d41fac53
Chandler Abril <abrilchandler1@gmail.com>
2024-06-25 19:12:43 -0700

    Done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
 
 def add(num1, num2):
-    pass
+    num1 = 45
+    num2 = 55
+    sum = num1 + num2
+    return sum
 
 def halve(number):
-    pass
+    half_of_number = number / 2
+    return half_of_number
+    
+    
